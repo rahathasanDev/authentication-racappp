422d44ef8dae23e9af6a151b2b3a47cfa8050eb1
rahathasanDev <devrahat.hasan@gmail.com>
2023-12-31 12:34:26 +0600

    components added

--- src/main.jsx
+++ src/main.jsx
@@ -16,5 +16,6 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
+    <App></App>
   </React.StrictMode>,
 )
